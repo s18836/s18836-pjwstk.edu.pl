1023273e44067b437de2bd262f35ec5120e5bded
s18836 <s18836@pjwstk.edu.pl>
2020-03-31 14:57:59 +0200

    cw4 1

---
+++ cw4/cw3/Controllers/StudentsController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using cw3.DAL;
+using cw3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw3.Controllers
+{
+
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+
+
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+            ListaStudentow l1 = new ListaStudentow();
+
+            using (var client = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18836;Integrated Security=True"))
+            using (var com = new SqlCommand())
+            {
+                Console.WriteLine("OTWIERAM");
+                com.Connection = client;
+                com.CommandText = "USE [2019SBD];SELECT * FROM STUDENT";
+
+                client.Open();
+                var dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    var st = new Student();
+                    st.FirstName = (dr["FirstName"].ToString());
+                    st.LastName = (dr["LastName"].ToString());
+                    st.IndexNumber = (dr["IndexNumber"].ToString());
+                    l1.dodaj(st);
+                }
+            }
+            
+
+            
+            return Ok(l1.getlist());
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            //... add to database
+            //... generating index number
+
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent()
+        {
+            return Ok("Aktualizacja ukonczona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent()
+        {
+            return Ok("Usuwanie zakonczone");
+        }
+
+    }
+}
---
+++ cw4/cw3/Models/ListaStudentow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw3.Models
+{
+    public class ListaStudentow
+    {
+        List<Student> lista = new List<Student>();
+
+        public void dodaj(Student st)
+        {
+            lista.Add(st);
+        }
+
+        public List<Student> getlist()
+        {
+            return lista;
+        }
+    }
+}
